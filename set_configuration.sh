2c5446010dea26495c92371e2e08dfe837de83c5
LinuxOperator <68796633+LinuxOperator@users.noreply.github.com>
2022-03-31 09:41:45 -0400

    Create set_configuration.sh

---
+++ set_configuration.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -x
+
+docker exec -u www-data nextcloud_app php occ --no-warnings config:system:get trusted_domains >> trusted_domain.tmp
+
+if ! grep -q "nextcloud_nginx" trusted_domain.tmp; then
+    TRUSTED_INDEX=$(cat trusted_domain.tmp | wc -l);
+    docker exec -u www-data nextcloud_app php occ --no-warnings config:system:set trusted_domains $TRUSTED_INDEX --value="nextcloud_nginx"
+fi
+
+rm trusted_domain.tmp
+
+docker exec -u www-data nextcloud_app php occ --no-warnings app:install onlyoffice
+
+docker exec -u www-data nextcloud_app php occ --no-warnings config:system:set onlyoffice DocumentServerUrl --value="/ds-vpath/"
+docker exec -u www-data nextcloud_app php occ --no-warnings config:system:set onlyoffice DocumentServerInternalUrl --value="http://nextcloud_onlyoffice/"
+docker exec -u www-data nextcloud_app php occ --no-warnings config:system:set onlyoffice StorageUrl --value="http://nextcloud_nginx/"
